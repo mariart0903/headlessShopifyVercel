f38bbe73272f1a9b44fb605607586d6b500570ed
Maria Radulescu <maria.radulescu@flipsidegroup.com>
2023-05-23 23:54:32 +0300

    Added vocal search2

--- components/Layout/Search.js
+++ components/Layout/Search.js
@@ -62,23 +62,23 @@ const Search = () => {
 		  <div className="bg-indigo-400 relative mx-auto flex-wrap flex items-center justify-between p-6 pt-0 lg:px-8">
 			  <div className="container mx-auto md:w-1/2">
 				  <div className="relative">
-					  <svg aria-hidden="true" className="absolute top-[10px] left-[10px] w-5 h-5 text-gray-500 dark:text-gray-400" fill="none"
+					  <svg aria-hidden="true" className="absolute top-[10px] left-[10px] md:w-5 md:h-5 h-7 w-7 text-gray-500 dark:text-gray-400" fill="none"
 						   stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
 						  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
 								d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
 					  </svg>
 					  <input
-						className=" px-[40px] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+						className=" px-[40px] pr-[65px] md:pr-[50px] bg-gray-50 border border-gray-300 text-gray-900 text-lg md:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 						type="text"
 						placeholder="Search..."
 						value={searchTerm}
 						onChange={handleChange}
 					  />
-					  <div onClick={ handleMicClick } className="absolute cursor-pointer top-[10px] right-[30px]">
-						  <MicrophoneIcon className={ `h-5 w-5 ${isListening ? 'text-gray-400' : ''}`}/>
+					  <div onClick={ handleMicClick } className="absolute cursor-pointer top-[10px] right-[40px] md:right-[30px]">
+						  <MicrophoneIcon className={ `md:h-5 md:w-5 h-7 w-7 ${isListening ? 'text-gray-400' : ''}`}/>
 					  </div>
 					  <button type="button" onClick={ handleClearSearch } className="absolute top-[16px] right-[10px] ">
-						  <svg className="w-3 h-3 text-gray-500 dark:text-gray-400" viewBox="0 0 460.775 460.775" fill="currentColor">
+						  <svg className="h-5 w-5 md:w-3 md:h-3 text-gray-500 dark:text-gray-400" viewBox="0 0 460.775 460.775" fill="currentColor">
 							  <path d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55
 		c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55
 		c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505
