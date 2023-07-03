3523c6c99ee700b838b285541fe0b9af13709890
Maria Radulescu <maria.radulescu@flipsidegroup.com>
2023-07-03 20:47:58 +0300

    Added vm on layout

--- components/Layout/layout.js
+++ components/Layout/layout.js
@@ -7,6 +7,7 @@ import {useDispatch} from "react-redux";
 import {setCheckout} from "../../store/checkoutSlice";
 import {useRouter} from "next/router";
 import dynamic from "next/dynamic";
+import {CHAT_ROUTE, CART_ROUTE, QUIZ_ROUTE} from "../../utils/constants";
 
 const Search = dynamic(() => import('./Search'), {ssr: false});
 const Auglio = dynamic(() => import('../../components/Parts/auglio/Auglio.js'), {ssr: false});
@@ -22,6 +23,13 @@ const Layout = ({children}) => {
 			window.yotpo.refreshWidgets();
 		}
 	}, [router, children]);
+
+	const showMirror = () => {
+		const {pathname} = router;
+		if (pathname !== CART_ROUTE && pathname !== CHAT_ROUTE && pathname !== QUIZ_ROUTE) return true;
+		return false;
+	};
+	const showMirrorWidget = showMirror();
 
 	useEffect(() => {
 		const checkout_id = localStorage.getItem("checkout_id");
@@ -61,7 +69,7 @@ const Layout = ({children}) => {
 				/*style={{backgroundImage: 'url("/images/lines2.png")'}}*/
 			>
 				<div className="fixed z-[999] bottom-[20px] right-[30px]">
-					<Auglio/>
+					{showMirrorWidget && <Auglio/>}
 				</div>
 				<div className={`${isHomePage ? '' : 'px-4 sm:px-6 lg:px-8'} container mx-auto`}>{children}</div>
 			</main>
